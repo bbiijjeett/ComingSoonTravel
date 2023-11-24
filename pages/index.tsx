c21aba9cd18278ea77fd19c8f3552542aca87a71
bbiijjeett <bijeetnathbtech@gmail.com>
2023-11-25 00:18:46 +0530

    first2 commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 import Image from "next/image";
-import { useMediaQuery } from "react-responsive";
 
 const IndexPage = () => (
   <Layout title="Coming Soon">
