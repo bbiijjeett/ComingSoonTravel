e37ac12a2e70232612a6625b0686c7d1aeac3acc
bbiijjeett <bijeetnathbtech@gmail.com>
2023-11-25 00:08:29 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,148 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
+
+const IndexPage = () => (
+  <Layout title="Coming Soon">
+    {/* <img
+      src="/images/xera.svg"
+      alt="Themeptation "
+      className="absolute h-96 -top-20 -right-16 lg:right-5 lg:top-10 animate-blob"
+    /> */}
+    {/* <img
+      src="/images/shapes.svg"
+      alt="hero"
+      className="absolute w-full left-24 bottom-24 animate-blob2"
+    /> */}
+    <div>
+      <Image src="/images/LOGO.png" width={150} height={150} alt="logo" />
+    </div>
+    <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-900">
+      <div className="text-center space-y-10">
+        <h3 className=" text-2xl uppercase tracking-wider font-bold">
+          Coming soon
+        </h3>
+        <h1 className="text-7xl lg:text-9xl font-extrabold fancy-undeline fancy-undeline:hover">
+          We’r blowing up
+        </h1>
+        <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto ">
+          We`re under construction. Check back for an update soon. Stay in
+          touch!
+        </p>
+      </div>
+      {/* <form className="relative z-10 mx-10 lg:max-w-xl lg:mx-auto">
+        <input
+          type="text"
+          placeholder="hello@amazingpuri.com"
+          className="w-full text-2xl font-light text-gray-900 placeholder-gray-500 py-5 pl-5 pr-36 lg:pr-44 rounded-xl"
+        />
+        <button className="absolute top-1 right-1 bottom-1 px-4 lg:px-10 text-xl font-semibold bg-gray-900 text-white rounded-xl transition ease-in-out duration-500 hover:bg-red-500">
+          Notify me
+        </button>
+      </form> */}
+    </div>
+    <div className=" absolute bottom-5">
+      <div className="backTab">
+        <p className="flex items-center justify-center text-lg font-semibold py-5 text-white ">
+          Follow us
+        </p>
+      </div>
+
+      <ul className="flex space-x-3">
+        <li>
+          <Link href="https://www.instagram.com/amazingpuri/">
+            <a className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="100"
+                height="100"
+                viewBox="0,0,256,256"
+              >
+                <g transform="translate(51.2,51.2) scale(0.6,0.6)">
+                  <g
+                    fill="#000000"
+                    fillRule="nonzero"
+                    stroke="none"
+                    strokeWidth="1"
+                    strokeLinecap="butt"
+                    strokeLinejoin="miter"
+                    strokeMiterlimit="10"
+                    strokeDasharray=""
+                    strokeDashoffset="0"
+                    fontFamily="none"
+                    fontWeight="none"
+                    fontSize="none"
+                    textAnchor="none"
+                  >
+                    <g transform="scale(5.12,5.12)">
+                      <path d="M16,3c-7.16752,0 -13,5.83248 -13,13v18c0,7.16752 5.83248,13 13,13h18c7.16752,0 13,-5.83248 13,-13v-18c0,-7.16752 -5.83248,-13 -13,-13zM16,5h18c6.08648,0 11,4.91352 11,11v18c0,6.08648 -4.91352,11 -11,11h-18c-6.08648,0 -11,-4.91352 -11,-11v-18c0,-6.08648 4.91352,-11 11,-11zM37,11c-1.10457,0 -2,0.89543 -2,2c0,1.10457 0.89543,2 2,2c1.10457,0 2,-0.89543 2,-2c0,-1.10457 -0.89543,-2 -2,-2zM25,14c-6.06329,0 -11,4.93671 -11,11c0,6.06329 4.93671,11 11,11c6.06329,0 11,-4.93671 11,-11c0,-6.06329 -4.93671,-11 -11,-11zM25,16c4.98241,0 9,4.01759 9,9c0,4.98241 -4.01759,9 -9,9c-4.98241,0 -9,-4.01759 -9,-9c0,-4.98241 4.01759,-9 9,-9z"></path>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.youtube.com/@AmazingPuri">
+            <a className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="100"
+                height="100"
+                viewBox="0,0,256,256"
+              >
+                <g transform="translate(51.2,51.2) scale(0.6,0.6)">
+                  <g
+                    fill="#000000"
+                    fillRule="nonzero"
+                    stroke="none"
+                    strokeWidth="1"
+                    strokeLinecap="butt"
+                    strokeLinejoin="miter"
+                    strokeMiterlimit="10"
+                    strokeDasharray=""
+                    strokeDashoffset="0"
+                    fontFamily="none"
+                    fontWeight="none"
+                    fontSize="none"
+                    textAnchor="none"
+                  >
+                    <g transform="scale(8.53333,8.53333)">
+                      <path d="M15,4c-4.186,0 -9.61914,1.04883 -9.61914,1.04883l-0.01367,0.01563c-1.90652,0.30491 -3.36719,1.94317 -3.36719,3.93555v6v0.00195v5.99805v0.00195c0.00384,1.96564 1.4353,3.63719 3.37695,3.94336l0.00391,0.00586c0,0 5.43314,1.05078 9.61914,1.05078c4.186,0 9.61914,-1.05078 9.61914,-1.05078l0.00195,-0.00195c1.94389,-0.30554 3.37683,-1.97951 3.37891,-3.94727v-0.00195v-5.99805v-0.00195v-6c-0.00288,-1.96638 -1.43457,-3.63903 -3.37695,-3.94531l-0.00391,-0.00586c0,0 -5.43314,-1.04883 -9.61914,-1.04883zM12,10.39844l8,4.60156l-8,4.60156z"></path>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.facebook.com/profile.php?id=61552074119189">
+            <a className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
